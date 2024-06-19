62c865bc347f090b8296e5ef4269750ca68eb67e
BiaSiriani <biasiriani200722@gmail.com>
2024-06-19 18:57:22 +0000

    adicionar pag

---
+++ perguntas.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Seleciona o botão "Responder"
+const answerButton = document.querySelector('.btn-answer');
+
+// Adiciona um evento de clique ao botão
+answerButton.addEventListener('click', () => {
+  // Aqui você pode adicionar a lógica para exibir a resposta ou redirecionar para a próxima pergunta
+  alert('Você respondeu a pergunta!');
+});
